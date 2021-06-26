6c425d574172ce9d2c95d8e0bf9c18a72524cdf4
Dmitriy Blyndaruk <dmitriy.blyndaruk@gmail.com>
2021-06-26 22:11:12 +0300

    update user tests

--- server/src/user/user.controller.spec.ts
+++ server/src/user/user.controller.spec.ts
@@ -1,5 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { getModelToken } from '@nestjs/mongoose';
+import { UserService } from './user.service';
+
+const mockUser = {
+  id: 909,
+  user_name: 'Lorem name',
+  user_email: 'lorem@gmail.com',
+  password: 'Some pass',
+}
 
 describe('UserController', () => {
   let controller: UserController;
@@ -7,6 +16,13 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: getModelToken('User'),
+          useValue: mockUser,
+        },
+        UserService,
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
--- server/src/user/user.service.spec.ts
+++ server/src/user/user.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { UserController } from './user.controller';
+import { User } from '../interfaces/user.interface';
+
+const mockUser = {
+  id: 909,
+  user_name: 'Lorem name',
+  user_email: 'lorem@gmail.com',
+  password: 'Some pass',
+}
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      controllers: [UserController],
+      providers: [
+        {
+          provide: getModelToken('User'),
+          useValue: mockUser,
+        },
+        UserService,
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
